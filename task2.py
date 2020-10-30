ba4dc3bb15635a6ed38e803947f08dd0555de40f
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-30 11:45:11 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -5,6 +5,6 @@ from tkinter import *
 window = tk.Tk()
 window.title("T-Town Veterinary Clinic Database")
 
-dogphoto = PhotoImage(file="dog.png")
-label1 = tk.Label(window, image=dogphoto)
-label2 = tk.Label(window, )
+dogphoto = PhotoImage(file = "dog.png")
+label1 = tk.Label(window, image = dogphoto)
+label2 = tk.Label(window, text = "Client Database")
