c468dfe456b39d9c3c9df7c92b0236f449531ac5
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-30 11:44:00 -0700

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,10 @@
+#!python3
+import tkinter as tk
+from tkinter import *
+
+window = tk.Tk()
+window.title("T-Town Veterinary Clinic Database")
+
+dogphoto = PhotoImage(file="dog.png")
+label1 = tk.Label(window, image=dogphoto)
+label2 = tk.Label(window, )
