6bf18f281b39b9052c7fe3386514e7282b16f4e9
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-30 11:44:17 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,11 +5,11 @@ from tkinter import *
 window = tk.Tk()
 window.title("tk")
 
-entry1 = tk.Entry(window,width = 10)
-entry2 = tk.Entry(window,width = 10)
-entry3 = tk.Entry(window,width = 20)
-label = tk.Label(window,text = "x")
-button = tk.Button(window,text = "=")
+entry1 = tk.Entry(window, width = 10)
+entry2 = tk.Entry(window, width = 10)
+entry3 = tk.Entry(window, width = 20)
+label = tk.Label(window, text = "x")
+button = tk.Button(window, text = "=")
 
 entry1.grid(row = 1, column = 1)
 label.grid(row = 1, column = 2)
