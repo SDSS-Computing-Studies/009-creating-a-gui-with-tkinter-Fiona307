2e3c7316a2928f2ca725ba97aabda51f01f8c52c
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-30 11:35:56 -0700

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,12 @@
+#!python3
+import tkinter as tk 
+from tkinter import *
+
+window = tk.Tk()
+
+entry1 = tk.Entry(window,width = 10)
+entry2 = tk.Entry(window,width = 10)
+entry3 = tk.Entry(window,width = 20)
+label = tk.Label(window,text = "x")
+button = tk.Button(window,text = "=")
+
