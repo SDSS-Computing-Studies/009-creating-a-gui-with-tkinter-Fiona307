ae7a2be8853918ccf7b08429540cda2f91288a73
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-11-02 11:13:02 -0800

    Update task3.py

--- task3.py
+++ task3.py
@@ -4,7 +4,7 @@ from tkinter import *
 
 window = tk.Tk()
 window.title("Example")
-window.geometry("260x140")
+window.geometry("256x131")
 
 dogphoto = PhotoImage(file="dog.png")
 label1 = tk.Label(window, image=dogphoto)
@@ -15,5 +15,6 @@ label2.place(x=153, y=50)
 
 label3 = tk.Label(window, text="A cuddly little puppy! this is from the same\ncreaters who brought you Keropi and Kero Kero", bg="#A3FFFF")
 label3.place(x=0, y=95)
+
 window.mainloop()
 
